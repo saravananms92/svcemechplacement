7c8d936b1c3873d609436e58a2ff30eef6d55c55
Dr. S. Saravanan <saravananms@svce.ac.in>
2025-12-30 13:01:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-const URL = "https://script.google.com/macros/s/AKfycbwiR2ryblrClwrfaQxal04c2jIUGknMhuqFIv4uF3QYo2T8HArG8Sou-Bp6XUVEGt4g/exec
-";
+const URL = "https://script.google.com/macros/s/AKfycbw6NW8cq8n4Wgk4gwTtqaDIQH9PtQxgLKCjLcPxQuQapm8jKEoN8uz8VjLagW18k8_k/exec";
 
 fetch(URL)
   .then(res => res.json())
@@ -44,3 +43,4 @@ function drawCompanyChart(stats) {
     }
   });
 }
+

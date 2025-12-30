7e92fa68918a40f3b360350be379d91f18aab872
Dr. S. Saravanan <saravananms@svce.ac.in>
2025-12-30 12:36:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+const URL = "https://script.google.com/macros/s/AKfycbwiR2ryblrClwrfaQxal04c2jIUGknMhuqFIv4uF3QYo2T8HArG8Sou-Bp6XUVEGt4g/exec
+";
+
+fetch(URL)
+  .then(res => res.json())
+  .then(data => {
+    updateKPIs(data.summary);
+    buildTable("studentTable", data.students);
+    buildTable("companyTable", data.companies);
+    drawCompanyChart(data.companyTypeStats);
+  });
+
+function updateKPIs(s) {
+  total.textContent = s.totalStudents;
+  placed.textContent = s.placed;
+  unplaced.textContent = s.unplaced;
+  percent.textContent = s.placementPercentage + "%";
+}
+
+function buildTable(id, rows) {
+  if (rows.length === 0) return;
+
+  const table = document.getElementById(id);
+  const thead = table.querySelector("thead");
+  const tbody = table.querySelector("tbody");
+
+  thead.innerHTML =
+    "<tr>" + Object.keys(rows[0]).map(h => `<th>${h}</th>`).join("") + "</tr>";
+
+  tbody.innerHTML = rows.map(r =>
+    "<tr>" + Object.values(r).map(v => `<td>${v}</td>`).join("") + "</tr>"
+  ).join("");
+}
+
+function drawCompanyChart(stats) {
+  new Chart(document.getElementById("companyChart"), {
+    type: "bar",
+    data: {
+      labels: Object.keys(stats),
+      datasets: [{
+        label: "Number of Companies",
+        data: Object.values(stats)
+      }]
+    }
+  });
+}

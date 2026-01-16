90ad444b1c2675bcd17a1de8c705057d6c0b3ba2
Dr. S. Saravanan <saravananms@svce.ac.in>
2026-01-17 00:26:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -69,9 +69,6 @@ function updateKPIs(data) {
   document.querySelector('#eligible strong').innerText = data.eligibleStudents || 0;
   document.querySelector('#placed strong').innerText = data.placedCount || 0;
   document.querySelector('#percentage strong').innerText = percent + "%";
-  document.querySelector('.cards')?.classList.add('loaded');
-  document.querySelector('.company-cards')?.classList.add('loaded');
-
 }
   const percent =
     data.optedStudents > 0
@@ -363,6 +360,7 @@ function downloadChart(chartId, filename) {
 
   img.src = url;
 }
+
 
 
 

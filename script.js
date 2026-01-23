1bda1ed8391fda04be0e224ab8796b45e937a962
Dr. S. Saravanan <saravananms@svce.ac.in>
2026-01-23 11:46:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -285,6 +285,8 @@ function populateStudentTable(data) {
   (data.placedStudents || []).forEach(s => {
     const tr = document.createElement('tr');
     tr.innerHTML = `
+      <td>${i + 1}</td>
+      <td>${s.registerNo}</td>
       <td>${s.programme}</td>
       <td>${s.name}</td>
       <td>${s.company}</td>
@@ -344,3 +346,4 @@ function downloadChart(chartId, filename) {
 
   img.src = url;
 }
+
